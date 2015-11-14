e00c80f42260e94f781bee7845dad74ace5ac994
Thilo Wischmeyer <thwischm@gmail.com>
2015-11-14 15:25:46 +0100

    lemonbar

---
+++ scripts/bar.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+padding=" "
+color_background="#33000000"
+color_foreground="#FFFFFF"
+color_accent="#FFFFFF"
+panel_height=32
+panel_font="Roboto:size=10"
+panel_font_bold="Roboto Medium:size=10"
+
+# Workspace infos
+workspaces() {
+  wm_infos=$(i3-msg -t get_workspaces)
+  workspaces=$(echo $wm_infos | jq '. | length')
+  wm_output=""
+
+  for ((i=0; i < $workspaces; i++))
+  do
+    name=$(echo $wm_infos | jq -r --arg i "$i" '.[$i | tonumber].name')
+    focused=$(echo $wm_infos | jq -r --arg i "$i" '.[$i | tonumber].focused')
+    urgent=$(echo $wm_infos | jq -r --arg i "$i" '.[$i | tonumber].urgent')
+    if [ "$urgent" = "true" ]; then
+      wm_output="$wm_output%{F$color_foreground}%{B$color_background}${padding}${name}${padding}%{B-}%{F-}"
+    elif [ "$focused" = "true" ]; then
+      wm_output="$wm_output%{F$color_foreground}%{B$color_background}%{U$color_accent}%{+u}${padding}${name}${padding}%{-u}%{B-}%{F-}"
+    else
+      wm_output="$wm_output%{F$color_foreground}%{B$color_background}${padding}${name}${padding}%{B-}%{F-}"
+    fi
+  done
+  echo "$wm_output"
+}
+
+clock() {
+  date '+%a. %H:%M'
+}
+
+{
+  while true; do
+
+    buf=""
+    buf="$buf %{l}%{T1}$(workspaces)"
+    buf="$buf %{c}%{T2}$(clock)"
+    echo $buf
+
+    sleep 0.5;
+  done
+} | lemonbar \
+  -g x"$panel_height" \
+  -f "$panel_font" \
+  -f "$panel_font_bold" \
+  -F "$color_foreground" \
+  -B "$color_background" \
+  -u 2
