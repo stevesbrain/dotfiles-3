2475eb21f488e9e9945c0db26faa6565a40b3778
Thilo Wischmeyer <thwischm@gmail.com>
2015-11-18 23:51:31 +0100

    window title

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -7,12 +7,13 @@ color_foreground="#FFFFFFFF"
 color_accent="#66FFFFFF"
 
 panel_height=24
-panel_font="Roboto Condensed:size=10"
+panel_font="Roboto:size=10"
 panel_font_bold="Roboto Medium:size=10"
+panel_font_condensed="Roboto Condensed:size=10"
 panel_icon_font="Material\-Design\-Iconic\-Font:style=Design-Iconic-Font:size=12"
 panel_icon_font_2="Material Design Icons:size=12"
 
-PANEL_FIFO=/tmp/panel-fifo
+panel_fifo=/tmp/panel-fifo
 
 # check if panel is already running
 if [[ $(pgrep -cx lemonbar) -gt 1 ]]; then
@@ -21,12 +22,15 @@ if [[ $(pgrep -cx lemonbar) -gt 1 ]]; then
 fi
 
 # remove old panel fifo, creat new one
-[ -e "$PANEL_FIFO" ] && rm "$PANEL_FIFO"
-mkfifo "$PANEL_FIFO"
+[ -e "$panel_fifo" ] && rm "$panel_fifo"
+mkfifo "$panel_fifo"
 
 icon() {
     echo -n -e "\u$1"
 }
+
+# Window Title
+xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/A\1/p' > "${panel_fifo}" &
 
 # Workspace infos
 while true; do
@@ -49,25 +53,25 @@ while true; do
   done
   echo "W$wm_output"
   sleep 0.1
-done > "$PANEL_FIFO" &
+done > "$panel_fifo" &
 
 # Clock
 while true; do
   datetime=$(date '+%a, %d. %B - %H:%M')
-  echo "C$datetime"
+  echo "C%{T2}$datetime"
   sleep 1
-done > "$PANEL_FIFO" &
+done > "$panel_fifo" &
 
 # Redshift
 while true; do
   read redhift_status < redshift-status
   if [ $redhift_status = "1" ]; then
-    echo "R${padding}%{T4}%{A:pkill -USR1 redshift && echo 0 > redshift-status:}$(icon f1c4)%{A}${padding}"
+    echo "R${padding}%{T5}%{A:pkill -USR1 redshift && echo 0 > redshift-status:}$(icon f1c4)%{A}${padding}"
   else
-    echo "R${padding}%{T4}%{A:pkill -USR1 redshift && echo 1 > redshift-status:}$(icon f1c5)%{A}${padding}"
+    echo "R${padding}%{T5}%{A:pkill -USR1 redshift && echo 1 > redshift-status:}$(icon f1c5)%{A}${padding}"
   fi
   sleep 0.2
-done > "$PANEL_FIFO" &
+done > "$panel_fifo" &
 
 # Volume
 while true; do
@@ -82,34 +86,29 @@ while true; do
   else
     volume_icon=$(icon f5fd)
   fi
-  echo "V${padding}%{T4}$volume_icon %{T1}$vol%${padding}"
+  echo "V${padding}%{T5}$volume_icon %{T3}$vol%${padding}"
   sleep 0.2
-done > "$PANEL_FIFO" &
+done > "$panel_fifo" &
 
 # Load
 while true; do
   avgload=$(cut -d " " -f 1-3 /proc/loadavg)
-  echo "L${padding}%{T4}$(icon f413) %{T1}$avgload${padding}"
+  echo "L${padding}%{T5}$(icon f413) %{T3}$avgload${padding}"
   sleep 1
-done > "$PANEL_FIFO" &
+done > "$panel_fifo" &
 
 # TODO: figure out how fifo works.
-# update_timer=0
-# update_count=0
-# updates() {
-#   ((update_timer++))
-#   if [ $update_timer -gt 10 ]; then
-#     update_count=$(pacman -Qu | wc -l)
-#     update_timer=0
-#   fi
-#   if [ $update_count -ne 0 ]; then
-#     echo "${padding}%{T4}$(icon f482) %{T1}$update_count Updates${padding}"
-#   else
-#     echo -n "$update_timer"
-#   fi
-# }
+while true; do
+  update_count=$(pacman -Qu | wc -l)
+  if [ $update_count -ne 0 ]; then
+    echo "U${padding}%{T5}$(icon f482) %{T3}$update_count Updates${padding}"
+  else
+    echo "U"
+  fi
+  sleep 900
+done > "$panel_fifo" &
 
-./bar_parser.sh < "$PANEL_FIFO" | lemonbar \
+./bar_parser.sh < "$panel_fifo" | lemonbar \
   -g x"$panel_height" \
   -F "$color_foreground" \
   -B "$color_background" \
@@ -117,6 +116,7 @@ done > "$PANEL_FIFO" &
   -o -1 \
   -f "$panel_font" \
   -f "$panel_font_bold" \
+  -f "$panel_font_condensed" \
   -f "$panel_icon_font" \
   -f "$panel_icon_font_2" | zsh
 
--- scripts/bar_parser.sh
+++ scripts/bar_parser.sh
@@ -2,6 +2,9 @@
 while read -r line ; do
 
   case $line in
+    A* )
+      title=$(xprop -id ${line#?} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+      ;;
     W* )
       wm_infos="${line#?}"
       ;;
@@ -17,8 +20,11 @@ while read -r line ; do
     L* )
       load="${line#?}"
       ;;
+    U* )
+      updates="${line#?}"
+      ;;
   esac
 
-  printf "%s\n" "%{l}$wm_infos%{c}$clock%{r}$volume$load$redshift_status"
+  printf "%s\n" "%{l}$wm_infos    $title%{c}$clock%{r}$updates$volume$load$redshift_status"
 
 done
